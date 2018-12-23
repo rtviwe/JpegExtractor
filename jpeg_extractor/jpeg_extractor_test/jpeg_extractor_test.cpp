014916ed1c5d39a75c53352de29a94057a463401
Igor <rtviwe@gmail.com>
2018-12-23 15:14:36 +0300

    Remove useless commentaries

--- jpeg_extractor/jpeg_extractor_test/jpeg_extractor_test.cpp
+++ jpeg_extractor/jpeg_extractor_test/jpeg_extractor_test.cpp
@@ -16,17 +16,13 @@ int main() {
     JpegExtractor jpeg_extractor(path2);
     jpeg_extractor.analyzeFile();
 
-    // Размер файла
     std::cout << "Размер файла: " << jpeg_extractor.getFileSize() << " байтов" << std::endl;
 
-    // Комментарий
     std::cout << "Комментарий: " << jpeg_extractor.getCommentary() << std::endl;
 
-    // Размеры изображения
     std::cout << "Высота: " << jpeg_extractor.getHeight() << std::endl;
     std::cout << "Ширина: " << jpeg_extractor.getWidth() << std::endl;
 
-    // Таблица квантования
     for (int i(0); i < jpeg_extractor.getQuantizationTables().size(); i++) {
         QuantizationTable qTable = jpeg_extractor.getQuantizationTables()[i];
         std::cout << "Таблица квантования #" << qTable.tableId << ":" << std::endl;
@@ -34,14 +30,12 @@ int main() {
         std::cout << qTable;
     }
 
-    // Компоненты
     for (int i(0); i < jpeg_extractor.getComponents().size(); i++) {
         std::cout << "Компонент #" << i << " " << jpeg_extractor.getComponents()[i] << std::endl;
     }
 
     std::cout << std::endl;
 
-    // Вывести все данные
     std::cout << jpeg_extractor << std::endl;
 
     return 0;
